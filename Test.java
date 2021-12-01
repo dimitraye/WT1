30ab7a3bee8461e12f4b712345b3e331831d10be
dimitraye <dimitraye94@gmail.com>
2021-12-01 14:43:56 +0100

    comments

---
+++ Test.java
@@ -0,0 +1,11 @@
+/**
+Comment
+*/
+package ma.enset;
+
+public class Test {
+    public static void main(Srting[] args) {
+        // -------------------
+        System.out.println("Hello!");
+    }
+}
